dc4e230a09c0316a3f08c4c9dbca9cc3bb739b61
SIM_RAITANI <simranraitani53@gmail.com>
2025-04-06 04:11:08 +0530

    Fetch pr controller established

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,6 +4,7 @@ import "./index.css";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
 import { ClerkProvider } from "@clerk/clerk-react";
+import '@ant-design/v5-patch-for-react-19';
 
 const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
 
